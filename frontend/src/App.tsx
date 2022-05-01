58e0ca4c35603db98b0f78c4220f8c01abf4a3ee
Hung Phan <phanhung1309@gmail.com>
2022-05-01 23:06:33 +0700

    Add react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <h1>eCommerce web application</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
